e3815c45383664ca57bc57ddc296d88c8b347f0f
Kristiyan Danailov <chrisdanbg@icloud.com>
2018-05-13 19:12:45 +0300

    Turn By Turn Navigation Was Enabled

--- toHomeMapBox/NavMapVC.swift
+++ toHomeMapBox/NavMapVC.swift
@@ -12,13 +12,34 @@ import MapboxCoreNavigation
 import MapboxDirections
 import MapboxNavigation
 
+import UIKit.UIGestureRecognizerSubclass
+
+private enum State {
+    case closed
+    case open
+}
+
+extension State {
+    var opposite: State {
+        switch self {
+        case .open: return .closed
+        case .closed: return .open
+        }
+    }
+}
 class NavMapVC: UIViewController, MGLMapViewDelegate {
     var mapView: NavigationMapView!
     var directionsRoute: Route?
+    var label: UILabel!
     
     var homeLocation = CLLocationCoordinate2D()
     var newHomeLocation = CLLocationCoordinate2D()
     var disneyLocation = CLLocationCoordinate2DMake(37.8014548, -122.4586558)
+    
+    private let popupOffset: CGFloat = 440
+    private let offset: CGFloat = 150
+    
+    private var bottomConstraint = NSLayoutConstraint()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,15 +48,91 @@ class NavMapVC: UIViewController, MGLMapViewDelegate {
         
         mapView.delegate = self
         
+        // Annotations
+      
+  
         mapView.setUserTrackingMode(.follow
             , animated: true)
         view.addSubview(mapView)
         mapView.showsUserLocation = true
         
-        addbutton()
+        layout()
+        /// Load Gesture Recognizers
+        littleView.addGestureRecognizer(littlepanRecognizer)
+        popupView.addGestureRecognizer(panRecognizer)
+        setHomeButton.addGestureRecognizer(setHomeRecognizer)
     }
     
-    func addbutton() {
+    // Views
+    
+    private lazy var popupView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
+        view.layer.shadowColor = UIColor.black.cgColor
+        view.layer.shadowOpacity = 0.1
+        view.layer.shadowRadius = 10
+        return view
+    }()
+    
+    
+    private lazy var littleView: UIView = {
+        let view = UIView ()
+        view.backgroundColor = .white
+        view.layer.shadowOpacity = 0.4
+        view.layer.shadowRadius = 10
+        view.layer.cornerRadius = 20.0
+        view.alpha = 1
+        view.layer.masksToBounds = true
+        view.layer.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+        return view
+    }()
+    
+    private lazy var overlayView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        view.alpha = 0
+        return view
+    }()
+    
+    private lazy var arrowImg: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = #imageLiteral(resourceName: "home-icon-silhouette")
+        return imageView
+    }()
+    
+    private lazy var closedTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Set My Home"
+        label.font = UIFont.systemFont(ofSize: 16, weight: UIFont.Weight.medium)
+        label.textColor = #colorLiteral(red: 0, green: 0.5898008943, blue: 1, alpha: 1)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private lazy var openTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Set My Home"
+        label.font = UIFont.systemFont(ofSize: 24, weight: UIFont.Weight.heavy)
+        label.textColor = .black
+        label.textAlignment = .center
+        label.alpha = 0
+        label.transform = CGAffineTransform(scaleX: 0.65, y: 0.65).concatenating(CGAffineTransform(translationX: 0, y: -15))
+        return label
+    }()
+    
+    private lazy var setHomeButton: UIButton = {
+        let newbutton = UIButton()
+        newbutton.setTitle("Set Pin As Your Home", for: .normal)
+        newbutton.layer.cornerRadius = 20.0
+        newbutton.clipsToBounds = true
+        newbutton.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+        newbutton.titleLabel?.font = UIFont(name: "AvenirNext-Bold", size: 18)
+        newbutton.showsTouchWhenHighlighted = true
+        newbutton.addTarget(self, action: #selector(setNewHome), for: .touchUpInside)
+        return newbutton
+    }()
+    func layout() {
         let toHomeButton: UIButton = {
             let button = UIButton(frame: CGRect(x: (view.frame.width/2) - 100, y: view.frame.height - 75, width:200, height: 50))
             button.setTitle("To Home", for: .normal)
@@ -51,8 +148,51 @@ class NavMapVC: UIViewController, MGLMapViewDelegate {
             button.addTarget(self, action: #selector(takeMeHome), for: .touchUpInside)
             return button
         }()
+        mapView.addSubview(toHomeButton)
         
-        view.addSubview(toHomeButton)
+        littleView.translatesAutoresizingMaskIntoConstraints = false
+        mapView.addSubview(littleView)
+        littleView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: -15).isActive = true
+        bottomConstraint = littleView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -32)
+        bottomConstraint.isActive = true
+        littleView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        littleView.widthAnchor.constraint(equalToConstant: 75).isActive = true
+    
+        setHomeButton.translatesAutoresizingMaskIntoConstraints = false
+        popupView.addSubview(setHomeButton)
+        setHomeButton.leadingAnchor.constraint(equalTo: popupView.leadingAnchor, constant: 60).isActive = true
+        setHomeButton.trailingAnchor.constraint(equalTo: popupView.trailingAnchor, constant: -60).isActive = true
+        setHomeButton.bottomAnchor.constraint(equalTo: popupView.bottomAnchor, constant: -370).isActive = true
+        setHomeButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        overlayView.translatesAutoresizingMaskIntoConstraints = false
+        mapView.addSubview(overlayView)
+        overlayView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        overlayView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        overlayView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        overlayView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        popupView.alpha = 0
+        popupView.translatesAutoresizingMaskIntoConstraints = false
+        mapView.addSubview(popupView)
+        popupView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        popupView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        bottomConstraint = popupView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: popupOffset)
+        bottomConstraint.isActive = true
+        popupView.heightAnchor.constraint(equalToConstant: 500).isActive = true
+        
+        closedTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        popupView.addSubview(closedTitleLabel)
+        closedTitleLabel.leadingAnchor.constraint(equalTo: popupView.leadingAnchor).isActive = true
+        closedTitleLabel.trailingAnchor.constraint(equalTo: popupView.trailingAnchor).isActive = true
+        closedTitleLabel.topAnchor.constraint(equalTo: popupView.topAnchor, constant: 20).isActive = true
+        
+        openTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        popupView.addSubview(openTitleLabel)
+        openTitleLabel.leadingAnchor.constraint(equalTo: popupView.leadingAnchor).isActive = true
+        openTitleLabel.trailingAnchor.constraint(equalTo: popupView.trailingAnchor).isActive = true
+        openTitleLabel.topAnchor.constraint(equalTo: popupView.topAnchor, constant: 30).isActive = true
+        
     }
   
     
@@ -91,10 +231,12 @@ class NavMapVC: UIViewController, MGLMapViewDelegate {
         }
     }
     
-  //  @objc func setNewHome(sender: UIButton!) {
-   //     newHomeLocation = homeLocation
-     //   storeCoordinates([newHomeLocation])
- //   }
+        @objc func setNewHome(sender: UIButton!) {
+           // newHomeLocation = homeLocation
+            //storeCoordinates([newHomeLocation])
+            self.calloutNavigationView()
+            
+        }
     
     @objc func buttonAction(sender: UIButton!) {
         UIView.animate(withDuration: 0.6,
@@ -134,10 +276,204 @@ class NavMapVC: UIViewController, MGLMapViewDelegate {
         let myHomeDestination = loadCoordinates()
         let currentLocation = mapView.userLocation!.coordinate
         calculateRoute(from: currentLocation, to: disneyLocation) { (route, error) in
+            
             if error != nil {
                 print("Error occured")
             }
         }
+        TimeRemainingLabel.appearance().isHidden = false
     }
 
+    
+    func calloutNavigationView(){
+        let navigateToRoute = directionsRoute!
+        let navigationVC = NavigationViewController(for: navigateToRoute)
+        self.present(navigationVC , animated: true, completion: nil)
+    }
+
+    /// Pan Animation
+    // MARK: - Animation
+    
+    /// The current state of the animation. This variable is changed only when an animation completes.
+    private var currentState: State = .closed
+    
+    /// All of the currently running animators.
+    private var runningAnimators = [UIViewPropertyAnimator]()
+    
+    /// The progress of each animator. This array is parallel to the `runningAnimators` array.
+    private var animationProgress = [CGFloat]()
+    
+    private lazy var panRecognizer: InstantPanGestureRecognizer = {
+        let recognizer = InstantPanGestureRecognizer()
+        recognizer.addTarget(self, action: #selector(popupViewPanned(recognizer:)))
+        return recognizer
+    }()
+    private lazy var littlepanRecognizer: InstantPanGestureRecognizer = {
+        let recognizer = InstantPanGestureRecognizer()
+        recognizer.addTarget(self, action: #selector(popupViewPanned(recognizer:)))
+        return recognizer
+    }()
+    private lazy var setHomeRecognizer: InstantPanGestureRecognizer = {
+        let recognizer = InstantPanGestureRecognizer()
+        recognizer.addTarget(self, action: #selector(setNewHome(sender:)))
+        return recognizer
+    }()
+    
+    
+    /// Animates the transition, if the animation is not already running.
+    private func animateTransitionIfNeeded(to state: State, duration: TimeInterval) {
+        
+        // ensure that the animators array is empty (which implies new animations need to be created)
+        guard runningAnimators.isEmpty else { return }
+        
+        // an animator for the transition
+        let transitionAnimator = UIViewPropertyAnimator(duration: duration, dampingRatio: 1, animations: {
+            switch state {
+            case .open:
+                self.bottomConstraint.constant = 280
+                self.popupView.layer.cornerRadius = 20
+                self.popupView.alpha = 1
+                self.overlayView.alpha = 0.5
+                self.closedTitleLabel.transform = CGAffineTransform(scaleX: 1.6, y: 1.6).concatenating(CGAffineTransform(translationX: 0, y: 15))
+                self.openTitleLabel.transform = .identity
+            case .closed:
+                
+                
+                self.bottomConstraint.constant = self.popupOffset
+                self.popupView.layer.cornerRadius = 0
+                self.popupView.alpha = 0
+                self.overlayView.alpha = 0
+                self.closedTitleLabel.transform = .identity
+                self.openTitleLabel.transform = CGAffineTransform(scaleX: 0.65, y: 0.65).concatenating(CGAffineTransform(translationX: 0, y: -15))
+            }
+            self.view.layoutIfNeeded()
+        })
+        
+        // the transition completion block
+        transitionAnimator.addCompletion { position in
+            
+            // update the state
+            switch position {
+            case .start:
+                self.currentState = state.opposite
+            case .end:
+                self.currentState = state
+            case .current:
+                ()
+            }
+            
+            // manually reset the constraint positions
+            switch self.currentState {
+            case .open:
+                self.bottomConstraint.constant = 280
+            case .closed:
+                self.bottomConstraint.constant = self.popupOffset
+                
+            }
+            
+            // remove all running animators
+            self.runningAnimators.removeAll()
+            
+        }
+        
+        // an animator for the title that is transitioning into view
+        let inTitleAnimator = UIViewPropertyAnimator(duration: duration, curve: .easeIn, animations: {
+            switch state {
+            case .open:
+                self.openTitleLabel.alpha = 1
+            case .closed:
+                self.closedTitleLabel.alpha = 1
+            }
+        })
+        inTitleAnimator.scrubsLinearly = false
+        
+        // an animator for the title that is transitioning out of view
+        let outTitleAnimator = UIViewPropertyAnimator(duration: duration, curve: .easeOut, animations: {
+            switch state {
+            case .open:
+                self.closedTitleLabel.alpha = 0
+            case .closed:
+                self.openTitleLabel.alpha = 0
+            }
+        })
+        outTitleAnimator.scrubsLinearly = false
+        
+        // start all animators
+        transitionAnimator.startAnimation()
+        inTitleAnimator.startAnimation()
+        outTitleAnimator.startAnimation()
+        
+        // keep track of all running animators
+        runningAnimators.append(transitionAnimator)
+        runningAnimators.append(inTitleAnimator)
+        runningAnimators.append(outTitleAnimator)
+        
+    }
+    
+    @objc private func popupViewPanned(recognizer: UIPanGestureRecognizer) {
+        switch recognizer.state {
+        case .began:
+            
+            // start the animations
+            animateTransitionIfNeeded(to: currentState.opposite, duration: 1)
+            
+            // pause all animations, since the next event may be a pan changed
+            runningAnimators.forEach { $0.pauseAnimation() }
+            
+            // keep track of each animator's progress
+            animationProgress = runningAnimators.map { $0.fractionComplete }
+            
+        case .changed:
+            
+            // variable setup
+            let translation = recognizer.translation(in: popupView)
+            var fraction = -translation.y / popupOffset
+            
+            // adjust the fraction for the current state and reversed state
+            if currentState == .open { fraction *= -1 }
+            if runningAnimators[0].isReversed { fraction *= -1 }
+            
+            // apply the new fraction
+            for (index, animator) in runningAnimators.enumerated() {
+                animator.fractionComplete = fraction + animationProgress[index]
+            }
+            
+        case .ended:
+            
+            // variable setup
+            let yVelocity = recognizer.velocity(in: popupView).y
+            let shouldClose = yVelocity > 0
+            
+            // if there is no motion, continue all animations and exit early
+            if yVelocity == 0 {
+                runningAnimators.forEach { $0.continueAnimation(withTimingParameters: nil, durationFactor: 0) }
+                break
+            }
+            
+            // reverse the animations based on their current state and pan motion
+            switch currentState {
+            case .open:
+                if !shouldClose && !runningAnimators[0].isReversed { runningAnimators.forEach { $0.isReversed = !$0.isReversed } }
+                if shouldClose && runningAnimators[0].isReversed { runningAnimators.forEach { $0.isReversed = !$0.isReversed } }
+            case .closed:
+                if shouldClose && !runningAnimators[0].isReversed { runningAnimators.forEach { $0.isReversed = !$0.isReversed } }
+                if !shouldClose && runningAnimators[0].isReversed { runningAnimators.forEach { $0.isReversed = !$0.isReversed } }
+            }
+            
+            // continue all animations
+            runningAnimators.forEach { $0.continueAnimation(withTimingParameters: nil, durationFactor: 0) }
+            
+        default:
+            ()
+        }
+    }
 }
+class InstantPanGestureRecognizer: UIPanGestureRecognizer {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent) {
+        if (self.state == UIGestureRecognizerState.began) { return }
+        super.touchesBegan(touches, with: event)
+        self.state = UIGestureRecognizerState.began
+    }
+    
+}
+
