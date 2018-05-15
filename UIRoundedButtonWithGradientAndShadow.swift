7110ceae58b5f7061a5b24cacb9d545392b9226e
Kristiyan Danailov <chrisdanbg@icloud.com>
2018-05-15 21:19:42 +0300

    Added Gradient / Modals anre more Fixes

---
+++ UIRoundedButtonWithGradientAndShadow.swift
@@ -0,0 +1,21 @@
+//
+//  UIRoundedButtonWithGradientAndShadow.swift
+//  toHomeMapBox
+//
+//  Created by Kristyan Danailov on 15.05.18 г..
+//  Copyright © 2018 г. Kristyan Danailov. All rights reserved.
+//
+
+import UIKit
+
+class UIRoundedButtonWithGradientAndShadow: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ toHomeMapBox/NoHomeModalVC.swift
@@ -0,0 +1,35 @@
+//
+//  NoHomeModalVC.swift
+//  toHomeMapBox
+//
+//  Created by Kristyan Danailov on 15.05.18 г..
+//  Copyright © 2018 г. Kristyan Danailov. All rights reserved.
+//
+
+import UIKit
+
+class NoHomeModalVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
